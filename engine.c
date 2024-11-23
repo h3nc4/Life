ef6a847efaa54b75cb9c72aedef77fff97b653ec
h3nc4 <me@h3nc4.com>
2024-11-23 14:03:26 -0300

    always run at 60 fps and migrate to unsigned types

--- engine.c
+++ engine.c
@@ -1,18 +1,19 @@
 /* See LICENSE file for copyright and license details.
  * Copyright (C) 2023-2024  Henrique Almeida <me@h3nc4.com> */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
 // Global grid
-static int *grid = NULL;
-static int *next_grid = NULL;
+static unsigned char *grid = NULL;
+static unsigned char *next_grid = NULL;
 
 // Grid dimensions
-static int width = 0;
-static int height = 0;
-static int size = 0; // width * height for flat array indexing
+static unsigned int width = 0;
+static unsigned int height = 0;
+static unsigned int size = 0; // width * height for flat array indexing
 
 // Macros for index calculation and neighbor counting
 #define IDX(y, x) (((y) * width + (x)))
@@ -25,35 +26,40 @@ static int size = 0; // width * height for flat array indexing
 	grid[IDX((y), PREV_X(x))] + grid[IDX((y), NEXT_X(x))] + \
 	grid[IDX(NEXT_Y(y), PREV_X(x))] + grid[IDX(NEXT_Y(y), (x))] + grid[IDX(NEXT_Y(y), NEXT_X(x))])
 
-// Aligned allocation for SIMD
-static void allocate_grids()
-{
-	grid = aligned_alloc(32, size * sizeof(int));
-	next_grid = aligned_alloc(32, size * sizeof(int));
-	memset(grid, 0, size * sizeof(int));
-	memset(next_grid, 0, size * sizeof(int));
-}
-
 void free_grids()
 {
 	free(grid);
 	free(next_grid);
 }
 
+// Aligned allocation for SIMD
+static void allocate_grids()
+{
+	grid = aligned_alloc(32, size * sizeof(char));
+	next_grid = aligned_alloc(32, size * sizeof(char));
+	if (!grid || !next_grid) {
+		fprintf(stderr, "Error: Failed to allocate memory for the grids.\n");
+		free_grids();
+		exit(EXIT_FAILURE);
+	}
+	memset(grid, 0, size * sizeof(char));
+	memset(next_grid, 0, size * sizeof(char));
+}
+
 static void init_grid()
 {
 	srand(time(NULL));
-	for (int i = 0; i < size; i++)
+	for (unsigned int i = 0; i < size; i++)
 		grid[i] = rand() % 2;
 }
 
 void update_grid()
 {
 #pragma omp parallel for schedule(static) // Parallelize loop
-	for (int idx = 0; idx < size; idx++)
+	for (unsigned int idx = 0; idx < size; idx++)
 	{
 		// Count living neighbors
-		int living_neighbors = COUNT_LIVING_NEIGHBORS(idx / width, idx % width);
+		char living_neighbors = COUNT_LIVING_NEIGHBORS(idx / width, idx % width);
 
 		// A cell becomes alive if it has exactly 3 neighbors (birth)
 		// A cell stays alive if it is currently alive and has exactly 2 neighbors (survival)
@@ -62,12 +68,12 @@ void update_grid()
 	}
 
 	// Swap grids
-	int *temp = grid;
+	unsigned char *temp = grid;
 	grid = next_grid;
 	next_grid = temp;
 }
 
-void initialize_game(int w, int h)
+void initialize_game(unsigned int w, unsigned int h)
 {
 	if (grid)
 		free_grids();
@@ -78,18 +84,18 @@ void initialize_game(int w, int h)
 	init_grid();
 }
 
-int **ptr_to_current_grid()
+unsigned char **ptr_to_current_grid()
 {
 	return &grid;
 }
 
 void clear_grid()
 {
-	memset(grid, 0, size * sizeof(int));
+	memset(grid, 0, size * sizeof(char));
 }
 
-void flip_cell_state(int y, int x)
+void toggle_cell_state(unsigned int y, unsigned int x)
 {
-	int idx = IDX(y, x);
+	unsigned int idx = IDX(y, x);
 	grid[idx] = 1 - grid[idx]; // Toggle between 0 and 1
 }
--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 # See LICENSE file for copyright and license details.
 # Copyright (C) 2023-2024  Henrique Almeida <me@h3nc4.com>
 
-from ctypes import CDLL, c_int, POINTER, cast
+from ctypes import CDLL, c_uint, c_ubyte, POINTER, cast
 from traceback import print_exc
 from argparse import ArgumentParser
 from os import getenv, environ
@@ -35,14 +35,14 @@ CELL_SIZE = args.cell_size
 
 # Load the shared library
 ENGINE = CDLL("./engine.so")
-ENGINE.initialize_game.argtypes = [c_int, c_int]
+ENGINE.initialize_game.argtypes = [c_uint, c_uint]
 ENGINE.initialize_game.restype = None
 ENGINE.update_grid.argtypes = []
 ENGINE.update_grid.restype = None
 ENGINE.ptr_to_current_grid.argtypes = []
-ENGINE.ptr_to_current_grid.restype = POINTER(POINTER(c_int))
-ENGINE.flip_cell_state.argtypes = [c_int, c_int]
-ENGINE.flip_cell_state.restype = None
+ENGINE.ptr_to_current_grid.restype = POINTER(POINTER(c_ubyte))
+ENGINE.toggle_cell_state.argtypes = [c_uint, c_uint]
+ENGINE.toggle_cell_state.restype = None
 ENGINE.free_grids.argtypes = []
 ENGINE.free_grids.restype = None
 ENGINE.clear_grid.argtypes = []
@@ -67,10 +67,17 @@ last_selected_cell = None
 GAME_SURFACE = pygame.Surface((WIDTH, HEIGHT))
 GAME_PIXELS = pygame.surfarray.pixels2d(GAME_SURFACE)
 ENGINE.initialize_game(WIDTH, HEIGHT)
-GRIDS_PTR_PTR = cast(ENGINE.ptr_to_current_grid(), POINTER(POINTER(c_int * (WIDTH * HEIGHT)))).contents
+GRIDS_PTR_PTR = cast(ENGINE.ptr_to_current_grid(), POINTER(POINTER(c_ubyte * (WIDTH * HEIGHT)))).contents
 
 def draw_grid():
-	"""Draw cells based on their current state."""
+	"""Draw cells based on their current state.
+
+	GRIDS_PTR_PTR is a pointer to a pointer to the current generation grid.
+
+	To access it, we need to dereference it twice.
+
+	First, we dereference the pointer to the pointer with [0], then we dereference the pointer to the grid and access it with [y * WIDTH + x].
+	"""
 	for y in range(HEIGHT):
 		for x in range(WIDTH):
 			state = GRIDS_PTR_PTR[0][y * WIDTH + x]  # Access directly from pointer
@@ -86,7 +93,7 @@ def handle_mouse_click_or_drag():
 	mx, my = pygame.mouse.get_pos()
 	x, y = mx // CELL_SIZE, my // CELL_SIZE
 	if 0 <= x < WIDTH and 0 <= y < HEIGHT and last_selected_cell != (x, y):  # Only toggle if this is a new cell
-		ENGINE.flip_cell_state(y, x)
+		ENGINE.toggle_cell_state(y, x)
 		last_selected_cell = (x, y)
 
 
@@ -117,16 +124,18 @@ def handle_events():
 
 
 running = True
+last_update_time = pygame.time.get_ticks()
+update_interval = 100
 try:
 	while running:
 		handle_events()  # Process events
-		if not paused:
+		current_time = pygame.time.get_ticks()
+		if current_time - last_update_time >= update_interval and not paused:
 			ENGINE.update_grid()
-			GAME_CLOCK.tick(10)  # Limit to 10 FPS when running
-		else:
-			GAME_CLOCK.tick(60)  # Increase to 60 FPS when paused for smoother interaction
+			last_update_time = current_time  # Update the last update time
 		draw_grid()  # Draw cells
 		pygame.display.flip()  # Update display
+		GAME_CLOCK.tick(60)  # Run at 60 FPS
 except Exception:
 	if getenv("DEBUG"):
 		print_exc()  # Print the full exception traceback
