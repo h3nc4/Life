90b8bf8a291d8b19f6fc5edab1d9e58b3775723f
h3nc4 <me@h3nc4.com>
2024-11-24 07:56:47 -0300

    create 'rules' struct

--- engine.c
+++ engine.c
@@ -6,27 +6,31 @@
 #include <string.h>
 #include <time.h>
 
-// Global grid
+// Global grid and its pointers
 static unsigned char *grid = NULL;
 static unsigned char *current_grid = NULL;
 static unsigned char *prev_grid = NULL;
-static unsigned char grid_state = 0; // 0 or 1
 
-// Grid dimensions
-static unsigned int width = 0;
-static unsigned int height = 0;
-static unsigned int size = 0; // width * height for flat array indexing
+typedef struct
+{
+	unsigned char grid_state; // Current grid state (0 or 1)
+	unsigned int width;		  // Grid width
+	unsigned int height;	  // Grid height
+	unsigned int size;		  // Total grid size (width * height)
+} rules;
+
+static rules game;
 
 // Macros for index calculation and neighbor counting
-#define IDX(y, x) (((y) * width + (x)))
-#define PREV_Y(y) (((y) - 1 + height) % height)
-#define NEXT_Y(y) (((y) + 1) % height)
-#define PREV_X(x) (((x) - 1 + width) % width)
-#define NEXT_X(x) (((x) + 1) % width)
-#define COUNT_LIVING_NEIGHBORS(y, x) \
+#define IDX(y, x) (((y) * game.width + (x)))
+#define PREV_Y(y) (((y) - 1 + game.height) % game.height)
+#define NEXT_Y(y) (((y) + 1) % game.height)
+#define PREV_X(x) (((x) - 1 + game.width) % game.width)
+#define NEXT_X(x) (((x) + 1) % game.width)
+#define COUNT_LIVING_NEIGHBORS(y, x)                                                                                \
 	(prev_grid[IDX(PREV_Y(y), PREV_X(x))] + prev_grid[IDX(PREV_Y(y), (x))] + prev_grid[IDX(PREV_Y(y), NEXT_X(x))] + \
-	prev_grid[IDX((y), PREV_X(x))] + prev_grid[IDX((y), NEXT_X(x))] + \
-	prev_grid[IDX(NEXT_Y(y), PREV_X(x))] + prev_grid[IDX(NEXT_Y(y), (x))] + prev_grid[IDX(NEXT_Y(y), NEXT_X(x))])
+	 prev_grid[IDX((y), PREV_X(x))] + prev_grid[IDX((y), NEXT_X(x))] +                                              \
+	 prev_grid[IDX(NEXT_Y(y), PREV_X(x))] + prev_grid[IDX(NEXT_Y(y), (x))] + prev_grid[IDX(NEXT_Y(y), NEXT_X(x))])
 
 void free_grid()
 {
@@ -36,7 +40,7 @@ void free_grid()
 // Aligned allocation for SIMD
 static void allocate_grid()
 {
-	grid = aligned_alloc(32, 2 * size * sizeof(char));
+	grid = aligned_alloc(32, 2 * game.size * sizeof(char));
 	if (!grid)
 	{
 		fprintf(stderr, "Error: Failed to allocate memory.\n");
@@ -47,37 +51,36 @@ static void allocate_grid()
 static void init_grid()
 {
 	srand(time(NULL));
-	for (unsigned int i = 0; i < size; i++)
+	for (unsigned int i = 0; i < game.size; i++)
 		grid[i] = rand() % 2;
-	memcpy(grid + size, grid, size * sizeof(char));
+	memcpy(grid + game.size, grid, game.size * sizeof(char));
 	current_grid = grid;
 }
 
 void update_grid()
 {
-	current_grid = grid + size * grid_state;
-	prev_grid = grid + size * (1 - grid_state);
+	current_grid = grid + game.size * game.grid_state;
+	prev_grid = grid + game.size * (1 - game.grid_state);
 #pragma omp parallel for schedule(static) // Parallelize loop
-	for (unsigned int idx = 0; idx < size; idx++)
+	for (unsigned int idx = 0; idx < game.size; idx++)
 	{
-		// Count living neighbors
-		char living_neighbors = COUNT_LIVING_NEIGHBORS(idx / width, idx % width);
+		char living_neighbors = COUNT_LIVING_NEIGHBORS(idx / game.width, idx % game.width);
 
 		// A cell becomes alive if it has exactly 3 neighbors (birth)
 		// A cell stays alive if it is currently alive and has exactly 2 neighbors (survival)
 		// Otherwise, it dies.
 		current_grid[idx] = (living_neighbors == 3) | (prev_grid[idx] & (living_neighbors == 2));
 	}
-	grid_state = 1 - grid_state;
+	game.grid_state = 1 - game.grid_state;
 }
 
 void initialize_game(unsigned int w, unsigned int h)
 {
 	if (grid)
 		free_grid();
-	width = w;
-	height = h;
-	size = width * height;
+	game.width = w;
+	game.height = h;
+	game.size = game.width * game.height;
 	allocate_grid();
 	init_grid();
 }
@@ -89,8 +92,8 @@ unsigned char **ptr_to_current_grid()
 
 void clear_grid()
 {
-	memset(grid, 0, size * sizeof(char));
-	grid_state = 0;
+	memset(grid, 0, game.size * sizeof(char));
+	game.grid_state = 0;
 }
 
 void toggle_cell_state(unsigned int y, unsigned int x)
