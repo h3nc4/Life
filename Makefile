ba1be108572630aab8ee3c97cc4f7207ce2b6616
h3nc4 <me@h3nc4.com>
2024-12-22 13:14:19 -0300

    add headers to source files

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
-# Compiler and flags
+# Copyright (C) 2023-2024  Henrique Almeida <me@h3nc4.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 CC = cc
 CFLAGS = -Wall -O3 -ftree-vectorize -ffast-math -fPIC -fopenmp $(DEBUG_FLAGS)
 LDFLAGS = -fopenmp -lX11 -lXinerama -lXext
--- life.c
+++ life.c
@@ -1,5 +1,19 @@
-/* See LICENSE file for copyright and license details.
- * Copyright (C) 2023-2024  Henrique Almeida <me@h3nc4.com> */
+/******************************************************************************
+	Copyright (C) 2023-2024  Henrique Almeida <me@h3nc4.com>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+******************************************************************************/
 
 #define UPDATE_FPS 10 // Frames per second for grid update
 #define FPS 60		  // Frames per second for rendering
@@ -362,12 +376,11 @@ int main(int argc, char *argv[])
 	#ifdef DEBUG_FPS_LOGGING
 	setbuf(stdout, NULL);
 	#endif
-	if (argc == 2)
-		if ((CELL_SIZE = atoi(argv[1])) <= 0)
-		{
-			fprintf(stderr, "Error: Cell size must be positive.\n");
-			return EXIT_FAILURE;
-		}
+	if (argc == 2 && (CELL_SIZE = atoi(argv[1])) <= 0)
+	{
+		fprintf(stderr, "Error: Cell size must be positive.\n");
+		return EXIT_FAILURE;
+	}
 	init_game();
 	start_game();
 	free_game();
